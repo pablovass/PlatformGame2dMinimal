4f2cef87caf0e77a21d891ae552c5fee6d64f679
pablovass <faktory.fm@live.com>
2021-03-10 12:08:02 -0300

    Scenes and Player done

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]  float speed=8f;
+    [SerializeField]  float speedJump=300f;
+    [SerializeField]  Rigidbody2D playerRb;
+    bool isGrounded = true;
+    
+     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //move the character from left to right with speed
+    playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
+    //condicional jump
+    if (isGrounded)
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            playerRb.AddForce(Vector2.up*speedJump);
+            isGrounded = false;
+        }
+    
+    }
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
